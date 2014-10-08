3897c4a607d635f6e770ca308bc79847e99509d5
bullno1 <thebullno1@gmail.com>
2014-10-08 17:37:19 +0800

    Allow path and message to be either string or binary

--- src/cowboy_reload.erl
+++ src/cowboy_reload.erl
@@ -3,12 +3,24 @@
 -export([reload/1, alert/1]).
 -export([start/2, stop/1]).
 
-reload(Path) -> gproc:send({p, l, {cowboy_reload, ws}}, {reload, Path}), ok.
+-spec reload(string() | binary()) -> ok.
+reload(Path) -> send_cmd(reload, Path).
 
-alert(Msg) -> gproc:send({p, l, {cowboy_reload, ws}}, {alert, Msg}), ok.
+-spec alert(string() | binary()) -> ok.
+alert(Msg) -> send_cmd(alert, Msg).
 
 % application
 
 start(_StartType, _StartArgs) -> cowboy_reload_sup:start_link().
 
 stop(_State) -> ok.
+
+% private
+
+send_cmd(CmdType, Param) when is_binary(Param) ->
+	gproc:send({p, l, {cowboy_reload, ws}}, {CmdType, Param}),
+	ok;
+
+send_cmd(CmdType, Param) when is_list(Param) ->
+	gproc:send({p, l, {cowboy_reload, ws}}, {CmdType, list_to_binary(Param)}),
+	ok.
